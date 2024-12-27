f3c65b2f4d75df4da75234f6d5fcd7b43894c96c
Nick Midmore <nmidmore@gmail.com>
2024-12-27 17:47:39 +0000

    add index method to category endpoint

--- coffee-shop-pos/Model/CategoryModel.cs
+++ coffee-shop-pos/Model/CategoryModel.cs
@@ -1,6 +1,15 @@
-﻿namespace coffee_shop_pos.Model;
+﻿
+using coffee_shop_pos.DataClasses;
+
+namespace coffee_shop_pos.Model;
 
 public class CategoryModel
 {
+    private readonly ShopContext _context;
 
+    public CategoryModel(ShopContext context)
+        => _context = context;
+
+    public List<Category> Index()
+        => _context.Categories.ToList(); 
 }
